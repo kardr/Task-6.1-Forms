0b44187fbb61e290cdc4c0b483179e8b02707cab
Карина Дроздова <K.Drozd33@yandex.ru>
2020-05-13 16:28:23 +0300

    .

--- Task 6.1 Forms/Form1.cs
+++ Task 6.1 Forms/Form1.cs
@@ -16,5 +16,32 @@ namespace Task_6._1_Forms
         {
             InitializeComponent();
         }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                int n = Convert.ToInt32(numericUpDown1.Value);
+                int[] mass = new int[n];
+                Random r = new Random();
+                for (int i = 0; i < n; i++)
+                {
+                    mass[i] = r.Next(-50, 50);
+                    textBox1.Text += mass[i] + " ";
+                }
+                Console.WriteLine();
+                for (int i = 1; i < n; i = i + 2)
+                {
+                    if (mass[i] > 0)
+                    {
+                        textBox2.Text += mass[i] + " ";
+                    }
+                }
+            }
+            catch (Exception E)
+            {
+                Console.WriteLine(E.Message);
+            }
+        }
     }
 }
